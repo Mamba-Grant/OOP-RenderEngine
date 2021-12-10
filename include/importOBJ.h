35a0ae272def4b43e3734b0fac2e3ea4a7216629
GrantSaggars1 <>
2021-12-10 19:22:20 +0000

    close to working

--- include/importOBJ.h
+++ include/importOBJ.h
@@ -8,24 +8,24 @@
 #pragma once
 
 #include "datatypes/geometry.h"
+#include <stdio.h>
 #include <iostream>
 #include <string.h>
 #include <fstream>
 #include <vector>
 
 // global
+std::vector<glm::vec3> tmp_vertices;
 std::vector<glm::vec2> tmp_UVs;
 std::vector<glm::vec3> tmp_normals;
-std::vector<unsigned int> tmp_vertexIndices;
-std::vector<unsigned int> tmp_uvIndices;
-std::vector<unsigned int> tmp_normalIndices;
+std::vector< unsigned int > tmp_vertexIndices, tmp_uvIndices, tmp_normalIndices;
 
 void import(char* filename, mesh* mesh) {
 
   mesh->name = filename;
 
   // open file 
-  FILE* file = fopen (filename, "r");
+  FILE* file = fopen(filename, "r");
   if (!file) {
     printf("Image could not be opened\n"); 
     return;
@@ -44,9 +44,11 @@ void import(char* filename, mesh* mesh) {
     if ( strcmp( lineHeader, "v" ) == 0 ){
         glm::vec3 vertexCoord;
         fscanf(file, "%f %f %f\n", &vertexCoord.x, &vertexCoord.y, &vertexCoord.z );
-        
-        vertex* fileVertex = new vertex{vertexCoord};
-        mesh->vertices.push_back(*fileVertex);
+
+      vertex* fileVertex = new vertex{vertexCoord};
+      mesh->vertices.push_back(*fileVertex);
+
+      //tmp_vertices.push_back(vertexCoord);
       }
     else if ( strcmp( lineHeader, "vt" ) == 0 ){
         glm::vec2 uv;
@@ -59,18 +61,20 @@ void import(char* filename, mesh* mesh) {
         tmp_normals.push_back(normal);
       }
     else if ( strcmp( lineHeader, "f" ) == 0 ){
-    unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-    fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+      unsigned int test;
+      static unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
 
-    tmp_vertexIndices.push_back(vertexIndex[0]);
-    tmp_vertexIndices.push_back(vertexIndex[1]);
-    tmp_vertexIndices.push_back(vertexIndex[2]);
-    tmp_uvIndices    .push_back(uvIndex[0]);
-    tmp_uvIndices    .push_back(uvIndex[1]);
-    tmp_uvIndices    .push_back(uvIndex[2]);
-    tmp_normalIndices.push_back(normalIndex[0]);
-    tmp_normalIndices.push_back(normalIndex[1]);
-    tmp_normalIndices.push_back(normalIndex[2]); 
+      fscanf(file, "%u/%u/%u %u/%u/%u %u/%u/%u\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+
+      tmp_vertexIndices.push_back(vertexIndex[0]);
+      tmp_vertexIndices.push_back(vertexIndex[1]);
+      tmp_vertexIndices.push_back(vertexIndex[2]);
+      tmp_uvIndices    .push_back(uvIndex[0]);
+      tmp_uvIndices    .push_back(uvIndex[1]);
+      tmp_uvIndices    .push_back(uvIndex[2]);
+      tmp_normalIndices.push_back(normalIndex[0]);
+      tmp_normalIndices.push_back(normalIndex[1]);
+      tmp_normalIndices.push_back(normalIndex[2]); 
     }
   }
 
@@ -87,13 +91,15 @@ void import(char* filename, mesh* mesh) {
     //    using the display function and obj file
 
     // log vertex progress print function:
+
+
     std::cout<<"v-"<<i+1<<": ";
     std::cout<<glm::to_string(mesh->vertices[tmp_vertexIndices[i]-1].position)<<std::endl;
     if ( (i+1) % 3 == 0) std::cout<<std::endl;
   }
   
   std::cout<<std::endl;
-
+  
   for (unsigned int i=0; i<tmp_uvIndices.size(); i++) {
     // set texcoord for each vertex
     mesh->vertices[tmp_uvIndices[i]-1].texcoord = tmp_UVs[tmp_uvIndices[i]-1];
@@ -115,7 +121,7 @@ void import(char* filename, mesh* mesh) {
     std::cout<<glm::to_string(mesh->vertices[tmp_normalIndices[i]-1].normal)<<std::endl;
     if ( (i+1) % 3 == 0) std::cout<<std::endl;
   }
-
+  
   printf("Finished Loading: %s:\n",mesh->name.c_str());
 
   return;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <GL/glew.h>
+//#include <GL/glew.h>
+#include "../include/GL/glew.h"
 #include "../include/freeglut-3.2.1/include/GL/freeglut.h"
 
 #include "../include/glm/glm.hpp"
@@ -8,9 +9,137 @@
 
 #include <iostream>
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
+
+
+  // build and compile our shader program
+  // ------------------------------------
+  // vertex shader
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  // check for shader compile errors
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  // fragment shader
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  // check for shader compile errors
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  // link shaders
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  // check for linking errors
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+  }
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+
+  float firstTriangle[] = {
+      -0.9f, -0.5f, 0.0f,  // left 
+      -0.0f, -0.5f, 0.0f,  // right
+      -0.45f, 0.5f, 0.0f,  // top 
+  };
+  float secondTriangle[] = {
+      0.0f, -0.5f, 0.0f,  // left
+      0.9f, -0.5f, 0.0f,  // right
+      0.45f, 0.5f, 0.0f   // top 
+  };
+
+  mesh* cube = new mesh;
+  import( (char*)"OBJs/cube.obj", cube );
+
+  unsigned int VBOs[1], VAOs[1], IBOs[1];
+  glGenVertexArrays(1, VAOs); // we can also generate multiple VAOs or buffers at the same time
+  glGenBuffers(1, VBOs);
+  glGenBuffers(1, IBOs);
+
+  glBindVertexArray(VAOs[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+  glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
+  
+  /*
+  glBindVertexArray(VAOs[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+  glBufferData(GL_ARRAY_BUFFER, cube->vertices.size() * sizeof(glm::vec3), &cube->vertices[0].position, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+  glEnableVertexAttribArray(0);
+  
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBOs[0]);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube->indices.size() * sizeof(unsigned int), &cube->indices[0], GL_STATIC_DRAW); 
+  */
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    // draw first triangle using the data from the first VAO
+    glBindVertexArray(VAOs[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    //glBindVertexArray(VAOs[0]);
+    //glDrawArrays(GL_TRIANGLES, 0, cube->indices.size());
+    //glBindVertexArray(0);
+
+    //glDrawElements(GL_TRIANGLES, cube->indices.size(), sizeof(unsigned int),0 );
+    
+
+
+
+  /*
+  mesh* suzanne = new mesh;
+  import((char*)"OBJs/suzanne.obj", cube);
+
+  generic_container* obj1 = new generic_container{"obj1", cube, NULL};
+  generic_container* obj2 = new generic_container {"obj2", suzanne, obj1};
+
+  std::cout<<obj2->parent_ptr->name;*/
+  //glfwTerminate();
 
   glFlush();
 }
@@ -37,61 +166,29 @@ void init() {
 
 
 int main(int argc, char** argv) {
-
+    
   // ready GLUT window:
-  
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowPosition(80, 80);
+  glutInitContextProfile(GLUT_CORE_PROFILE);
+  glutInitContextVersion(3,3);
   
   // 16:9 ratio
   glutInitWindowSize(512, 576/2);
-  glutCreateWindow("Geometry Testing");
+  glutCreateWindow("This is a Monkey (if you could not tell)");
 
   // initialize GLEW
   glewInit();
-  
-  mesh* cube = new mesh;
-  import((char*)"OBJs/cube.obj", cube);
 
-  float vertices[] = {
-     0.0f,  0.5f, // Vertex 1 (X, Y)
-     0.5f, -0.5f, // Vertex 2 (X, Y)
-    -0.5f, -0.5f  // Vertex 3 (X, Y)
-  };
-
-  GLuint vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-
-  //glDrawArrays( GL_TRIANGLES, 0, suzanne->vertices.size() );
-
-  GLuint vbo;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  
-  //glBufferData(GL_ARRAY_BUFFER, suzanne->vertices.size() * sizeof(glm::vec3), &suzanne->vertices[0].position, GL_STATIC_DRAW);
-
-  glutReshapeFunc(reshape);
+  // display things
+  //glutReshapeFunc(reshape);
   glutDisplayFunc(display);
-  init();
+  //init();
   glutMainLoop();
-  
-  //std::cout<<sizeof(glm::vec3)<<std::endl;
-  //std::cout<<glm::to_string(cube->vertices[0].position)<<std::endl
-/*
-  mesh* suzanne = new mesh;
-  import((char*)"OBJs/suzanne.obj", cube);
 
-  generic_container* obj1 = new generic_container{"obj1", cube, NULL};
-  generic_container* obj2 = new generic_container {"obj2", suzanne, obj1};
-
-  std::cout<<obj2->parent_ptr->name;*/
-  //glfwTerminate();
   return 0;
 }
 
-// OpenGL thingy: 
+// OpenGL bufferdata setup: 
 //glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
